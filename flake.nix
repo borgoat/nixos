a533a8ab3997b15db51f9ed9727c96047d3b355f
Giorgio <giorgio@azzinna.ro>
2022-03-06 11:46:47 +0100

    feat: enable Home Manager

--- flake.nix
+++ flake.nix
@@ -1,8 +1,23 @@
 {
-  outputs = { self, nixpkgs }: {
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs, home-manager }: {
     nixosConfigurations.tower = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
-      modules = [ ./configuration.nix ];
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.borgoat = import ./home.nix;
+        }
+      ];
     };
   };
 }
