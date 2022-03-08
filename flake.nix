3d8ccb3980e9c85541ef5a7502be457cb230d140
Giorgio <giorgio@azzinna.ro>
2022-03-08 12:49:31 +0100

    feat: add macbook

--- flake.nix
+++ flake.nix
@@ -19,6 +19,18 @@
         }
       ];
     };
+    nixosConfigurations.macbook = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./machines/macbook/configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.borgoat = import ./home.nix;
+        }
+      ];
+    };
   };
 }
 
