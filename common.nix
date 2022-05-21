8f68cc4ba37e8f351b7cf4a8197fa8074db37fbf
Giorgio <giorgio@azzinna.ro>
2022-05-21 15:18:19 +0200

    split graphical config

--- common.nix
+++ common.nix
@@ -43,19 +43,6 @@
   #   keyMap = "us";
   # };
 
-  # Enable the X11 windowing system.
-  services.xserver = {
-    enable = true;
-
-    # Enable the GNOME Desktop Environment.
-    displayManager.gdm.enable = true;
-    desktopManager.gnome.enable = true;
-
-    # Keyboards
-    layout = "us,it";
-  };
-  
-
   # Configure keymap in X11
   # services.xserver.layout = "us";
   # services.xserver.xkbOptions = "eurosign:e";
@@ -64,26 +51,6 @@
   services.printing = {
     enable = true;
     drivers = [ pkgs.brlaser ];
-  };
-
-  # Enable sound.
-  sound.enable = false;
-  hardware.pulseaudio.enable = false;
-
-  # Remove sound.enable or turn it off if you had it set previously, it seems to cause conflicts with pipewire
-  # rtkit is optional but recommended
-  security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -109,25 +76,13 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
-    android-studio
     android-tools
     du-dust  # A more intuitive version of du in rust
     duf  # A better df alternative
     file  # A program that shows the type of files
-    firefox
     git
-    gnome3.gnome-tweaks
-    gnomeExtensions.espresso
     hexyl  # A command-line hex viewer
-    jetbrains.clion
-    jetbrains.goland
-    jetbrains.idea-ultimate
-    jetbrains.pycharm-professional
-    jetbrains.webstorm
-    keepassxc
     neovim
-    signal-desktop
-    zoom-us
   ];
 
   programs.adb.enable = true;
@@ -231,14 +186,6 @@
       enableOnBoot = false;
     };
   };
-
-  fonts.fonts = with pkgs; [
-    noto-fonts
-    fira-code
-    fira-code-symbols
-    jetbrains-mono
-    meslo-lgs-nf
-  ];
 
 }
 
