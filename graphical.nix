dd17d4369f5f26214d96c479156170c3ca992e86
Giorgio Azzinnaro <giorgio@azzinna.ro>
2024-10-30 16:43:56 +0100

    feat: 1password

--- graphical.nix
+++ graphical.nix
@@ -51,6 +51,15 @@
     keepassxc
   ];
 
+  # https://nixos.wiki/wiki/1Password
+  programs._1password.enable = true;
+  programs._1password-gui = {
+    enable = true;
+    # Certain features, including CLI integration and system authentication support,
+    # require enabling PolKit integration on some desktop environments (e.g. Plasma).
+    polkitPolicyOwners = [ "borgoat" ];
+  };
+
   fonts = {
     packages = with pkgs; [
       corefonts
